acabf933e64736a23d1f15de53836d3fe500c6fd
Ramiro Nocelli <rnocelli@bmat.com>
2025-04-11 16:20:03 +0200

    add tests

---
+++ src/balances.rs
@@ -0,0 +1,47 @@
+use std::collections::BTreeMap;
+
+pub struct Pallet {
+	// A simple storage mapping from accounts (`String`) to their balances (`u128`).
+	balances: BTreeMap<String, u128>,
+}
+
+impl Pallet {
+	pub fn new() -> Self {
+		Self { balances: BTreeMap::new() }
+	}
+
+	pub fn set_balance(&mut self, who: &String, amount: u128) {
+		self.balances.insert(who.clone(), amount);
+	}
+
+	pub fn balance(&self, who: &String) -> u128 {
+		*self.balances.get(who).unwrap_or(&0)
+	}
+
+	pub fn transfer(
+		&mut self,
+		caller: String,
+		to: String,
+		amount: u128,
+	) -> Result<(), &'static str> {
+		Ok(())
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn init_balances() {
+		let mut balances = super::Pallet::new();
+
+		assert_eq!(balances.balance(&"alice".to_string()), 0);
+		balances.set_balance(&"alice".to_string(), 100);
+		assert_eq!(balances.balance(&"alice".to_string()), 100);
+		assert_eq!(balances.balance(&"bob".to_string()), 0);
+	}
+
+	#[test]
+	fn transfer_balance() {
+	}
+}
+
